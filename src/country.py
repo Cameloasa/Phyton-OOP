cfc3f1c607db98fb5c3708cd5756cc27fd9a6545
Camelia Ciuca <camelia1414@gmail.com>
2025-02-10 17:59:39 +0100

    countries, print_info, add_language

--- src/country.py
+++ src/country.py
@@ -1,9 +1,20 @@
 class Country:
-    def __init__(self, name, pop):
+    def __init__(self, name, pop, area=None,):
         self.__name = name
         self.__population = pop
+        self.__area = area
+        self.__languages = []
 
 
     def print_info(self):
         info = f"I {self.__name} bor det {self.__population} miljoner invånare."
+        if self.__area is not None:
+            info += f" Landets area är {self.__area} km²."
         print(info)
+
+        if self.__languages:
+            print("Officiella språk:", ", ".join(self.__languages))
+
+
+    def add_language(self, language):
+        self.__languages.append(language)
--- src/main.py
+++ src/main.py
@@ -10,7 +10,21 @@ my_user.is_correct_login("Cami","1234")
 
 print("************")
 print("Countries")
-se = Country("Sverige", 10.5)
-no = Country("Norge", 5.5)
+# Skapa objekt för Sverige, Norge, Island och Danmark
+se = Country("Sverige", 10.5, 447425)
+no = Country("Norge", 5.5, 385207)
+island = Country("Island", 0.38, 103000)
+danmark = Country("Danmark", 6.0, 42924)
+
+# Lägg till språk
+se.add_language("Svenska")
+no.add_language("Norska")
+no.add_language("Svenska")
+island.add_language("Isländska")
+danmark.add_language("Danska")
+
+# Testa print_info-metoden
 se.print_info()
 no.print_info()
+island.print_info()
+danmark.print_info()
